c735e2d9473771d3442927914bd6bdc98f4da144
indicamedapp-web <indicamed.app@gmail.com>
2026-07-21 17:58:45 -0400

    api

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Reemplaza únicamente estos dos valores al crear el proyecto.
 window.INDICAMED_CONFIG = {
   SUPABASE_URL: "https://wldhaejgmmvciknkwjpy.supabase.co",
-SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZGhhZWpnbXZjaWtua3dqcHkiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTY0cCI6MjEwMDIzNzEzNzE0Nn0.j1_g8IRmopW013YbpTiM5QYOPf_HoxHSmj0UngiyLSI"
+SUPABASE_ANON_KEY: "sb_publishable_ifibPk2--uMW0fs1oFoHkg_KNHbeHjF"
 };
